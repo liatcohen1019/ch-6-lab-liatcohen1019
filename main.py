9b3efcded2a3ad4a0ad0577b83f0a53622d45a54
Liatcohen1019 <98411861+liatcohen1019@users.noreply.github.com>
2022-03-29 00:49:56 +0000

    final

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
+import turtle 
 
 def seq3np1(n):
-    """
-        Print the 3n+1 sequence from n, terminating when it reaches 1.
-        args: n (int) starting value for 3n+1 sequence
-        return: None
-    """
-    while(n != 1):
-        print(n)
-        if(n % 2) == 0:        # n is even
-            n = n // 2
-        else:                 # n is odd
-            n = n * 3 + 1
-    print(n)                  # the last print is 1
-
+  count = 0
+  while(n != 1):
+      count= count +1
+      if(n % 2) == 0:        
+        n = n // 2
+      else:                
+        n = n * 3 + 1
+      #print(count)
+  return count
+  
+def graph(upper_bound): 
+  T1= turtle.Turtle()
+  T2 = turtle.Turtle()
+  wn=turtle.Screen()
+  wn.setworldcoordinates(0,0,10,10)
+  max_so_far= 0
+  T2.goto(0,10)
+  
+  for x in range(1, upper_bound+1): 
+    result= seq3np1(x)
+    if result> max_so_far: 
+      max_so_far = result
+    T2.goto(0,max_so_far)
+    string= "maximim so far:", x, result 
+    T2.clear()
+    T2.write(string)
+    wn.setworldcoordinates(0,0, x+10, max_so_far+10)
+    T1.goto(x, result)
+  wn.exitonclick()    
+  
 def main():
-	seq3np1(3)
+  upper_bound = int(input("upper bound:"))
+  if(upper_bound < 0):
+    return
+  for start in range(1, upper_bound+1):
+    seq3np1(upper_bound)
+    print("Current loop value: ", start, "Number of iterations: ", seq3np1(start))
+  graph(upper_bound)
+  seq3np1(3)
 main()
